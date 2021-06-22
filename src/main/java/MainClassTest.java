5a6381ae942e13d35872a3356841122dae454a53
AKhaybulina <41270955+AKhaybulina@users.noreply.github.com>
2021-06-22 17:07:49 +0300

    Тест 2

--- src/main/java/MainClassTest.java
+++ src/main/java/MainClassTest.java
@@ -6,4 +6,9 @@ public class MainClassTest extends MainClass {
     public void testGetLocalNumber() {
         Assertions.assertTrue(getLocalNumber() == 14, "Вернулось число " + getLocalNumber() + ", а не 14");
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assertions.assertTrue(getClassNumber() > 45, "Вернулось число " + getClassNumber() + ", а это меньше 45");
+    }
 }
