91e2b2caeace06c817177c34ff1246c3480a06d4
AKhaybulina <41270955+AKhaybulina@users.noreply.github.com>
2021-07-20 14:47:02 +0300

    Ex7*: Поворот экрана
    Перенесла orientation в capabilities

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -30,13 +30,13 @@ public class FirstTest {
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "/Users/akhaybulina/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("orientation", "PORTRAIT");
 
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @After
     public void tearDown() {
-        driver.rotate(ScreenOrientation.PORTRAIT);
         driver.quit();
     }
 
